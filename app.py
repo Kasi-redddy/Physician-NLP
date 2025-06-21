d37ace8a1187341c36a4c2c7e13a2aeb733adc94
kasi visweswar reddy <reddykasivisweswar@gmail.com>
2025-06-21 17:59:07 +0530

    app.py

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import streamlit as st
 import re
 import spacy
 from transformers import pipeline
+import os
+os.system("python -m spacy download en_core_web_sm")
+
 
 st.set_page_config(page_title="Physician Notetaker", layout="wide")
 
